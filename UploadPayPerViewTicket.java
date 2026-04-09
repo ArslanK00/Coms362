ae8402f2010557c1726fcda07982f39569abe05f
ArslanK00 <arslan.a.khan26@gmail.com>
2026-04-09 13:33:12 -0500

    Working UploadPayperViewTicket

--- UploadPayPerViewTicket.java
+++
@@ -1,6 +0,0 @@
-public class UploadPayPerViewTicket {
-    public static void main(String[] args) {
-        // Your code starts here
-        System.out.println("Hello, Java!");
-    }
-}
---
+++ src/Objects/Ticket.java
@@ -0,0 +1,20 @@
+package src.Objects;
+import java.time.YearMonth;
+
+public class Ticket {
+    private static int nextId = 1;
+    int id;
+    String event;
+    double price;
+    String TicketType;
+    YearMonth EventDate;
+    
+    public Ticket(String event, double price, String ticketType, YearMonth eventDate) {
+        this.id = nextId++;
+        this.event = event;
+        this.price = price;
+        this.TicketType = ticketType;
+        this.EventDate = eventDate;
+    }
+
+}
