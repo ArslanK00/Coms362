e805c74e4fa077403778421f64c262377db55c36
ArslanK00 <arslan.a.khan26@gmail.com>
2026-04-16 16:57:49 -0500

    MerchandiseSales can return all Merchandise as records

--- src/MerchTest.java
+++ src/MerchTest.java
@@ -3,7 +3,9 @@ import Objects.RecordTypes.MerchandiseSales;
 public class MerchTest {
     public static void main(String[] args) 
     {
-        MerchandiseSales merchSales = new MerchandiseSales();
+        //UploadMerchandiseSales merchSales = new UploadMerchandiseSales();
+        MerchandiseSales merchsales = new MerchandiseSales("T-shirt", 20.0f, java.time.YearMonth.of(2020, 10));
+        merchsales.TurnAllMerchandiseToSystemRecords(true);
     }
 
 }
