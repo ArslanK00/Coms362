552224aeea020c4753e105419db9a8d84f5221e5
TheShauceGod <118031824+TheShauceGod@users.noreply.github.com>
2026-04-21 15:19:57 -0500

    Revert "Iteration 2 code"

--- src/Objects/RecordTypes/PayPerViewTicket.java
+++ src/Objects/RecordTypes/PayPerViewTicket.java
@@ -1,82 +1,26 @@
 package Objects.RecordTypes;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.time.YearMonth;
+public class PayPerViewTicket extends AbstractRecord{
+    private RecordEnum recordType = RecordEnum.PayPerViewTicket;
 
-public int amount;
-
-public class PayPerViewTicket implements Record
-{
-    private String name;
-    private float cost;
-    private YearMonth date;
-    String FilePathToRecords = "Databases/Records.txt";
-    public PayPerViewTicket(String name, float cost, YearMonth date, int amount) 
-    {
-        this.name = name;
-        this.cost = cost;
-        this.date = date;
-        this.amount = amount;
-    }
-
-
-    @Override
-    public YearMonth getDate() 
-    {
-        return date;
+    public PayPerViewTicket(int id, String name){
+        super(name);
     }
 
     @Override
-    public float getCost() {
-        return cost;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-    
-    public PayPerViewTicket[] TurnAllPayPerViewTicketRecords(boolean SendtoRecords)
-    {
+    public String toString(){
+        String summary = "Record Type: " + recordType + "\n"
+        + "Name: " + name + "\n"
+        + "Cost: " + cost + "\n"
+        + "Date: " + date + "\n";
         
-        UploadPayPerViewTicketSales uploadPayPerViewTicket = new UploadPayPerViewTicketSales();
-        String[][] payPerViewTicketData = uploadPayPerViewTicket.getPayPerViewTicket();
-        PayPerViewTicketSales[] PayPerViewTicketSystemRecords = new PayPerViewTicketSales[uploadPayPerViewTicket.getPayPerViewTicket().length];
-        try
-        {
-            FileWriter Writer = new FileWriter(FilePathToRecords, true);
-            for (int i = 0; i < payPerViewTicketData.length; i++) 
-            {
-
-
-                float tempCost = (Float.parseFloat(payPerViewTicketData[i][3]) * Float.parseFloat(payPerViewTicketData[i][4])) - (Float.parseFloat(payPerViewTicketData[i][3]) * Float.parseFloat(payPerViewTicketData[i][5]));
-                PayPerViewTicketSales payPerViewTicketSale = new PayPerViewTicketSales(payPerViewTicketData[i][2].trim(),tempCost,YearMonth.parse(payPerViewTicketData[i][6].trim()));
-                PayPerViewTicketSystemRecords[i] = payPerViewTicketSale;
-                if(SendtoRecords)
-                {
-                    Writer.write(PayPerViewTicketSystemRecords[i].name + ", " + PayPerViewTicketSystemRecords[i].cost + ", " + PayPerViewTicketSystemRecords[i].date + "\n");
-                    
-                }
-                else 
-                {
-                    System.out.println(PayPerViewTicketSystemRecords[i].name + ", " + PayPerViewTicketSystemRecords[i].cost + ", " + PayPerViewTicketSystemRecords[i].date);
-                }
-            }
-            Writer.close();
-            System.out.println("Successfully retrieved all live-event ticket Records");
-        }
-        catch (IOException ex) 
-        {
-            System.out.println("IOException is caught");
+        if(description == null || description.length() == 0){
+            return summary;
         }
 
-        return PayPerViewTicketSystemRecords;
-
+        summary += "Description: " + description + "\n";
+        return summary;
     }
+
 
 }
