4618ebc3d60f1d82395d5ec871a601c46688d787
ArslanK00 <arslan.a.khan26@gmail.com>
2026-04-21 16:00:52 -0500

    Serializable

--- src/MerchTest.java
+++ src/MerchTest.java
@@ -7,6 +7,7 @@ public class MerchTest {
         MerchandiseSale merchsales = new MerchandiseSale("T-shirt", 20.0f, java.time.YearMonth.of(2020, 10));
         merchsales.TurnAllMerchandiseToRecords(true);
         merchsales.EditMerchandiseDatabase();
+        //merchsales.readRecords();
     }
 
 }
--- src/Objects/RecordTypes/MerchandiseSale.java
+++ src/Objects/RecordTypes/MerchandiseSale.java
@@ -1,12 +1,17 @@
 package Objects.RecordTypes;
 
-import java.io.FileWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.time.YearMonth;
+import java.util.ArrayList;
 
 
 
-public class MerchandiseSale implements Record
+public class MerchandiseSale implements Record, Serializable
 {
     private String name;
     private float cost;
@@ -42,42 +47,96 @@ public class MerchandiseSale implements Record
     }
 
     
-    public MerchandiseSale[] TurnAllMerchandiseToRecords(boolean SendtoRecords)
-    {
+//     public MerchandiseSale[] TurnAllMerchandiseToRecords(boolean SendtoRecords) Just adding to text file
+//     {
         
-        MerchandiseController uploadMerch = new MerchandiseController(false);
-        String[][] merchData = uploadMerch.getMerch();
-        MerchandiseSale[] MerchSystemRecords = new MerchandiseSale[uploadMerch.getMerch().length];
-        try
-        {
-            FileWriter Writer = new FileWriter(FilePathToRecords, true);
-            for (int i = 0; i < merchData.length; i++) 
-            {
-
-
-                float tempCost = (Float.parseFloat(merchData[i][3]) * Float.parseFloat(merchData[i][4])) - (Float.parseFloat(merchData[i][3]) * Float.parseFloat(merchData[i][5]));
-                MerchandiseSale merchSale = new MerchandiseSale(merchData[i][2].trim(),tempCost,YearMonth.parse(merchData[i][6].trim()));
-                MerchSystemRecords[i] = merchSale;
-                if(SendtoRecords)
-                {
-                    Writer.write(MerchSystemRecords[i].name + ", " + MerchSystemRecords[i].cost + ", " + MerchSystemRecords[i].date + "\n");
+//         MerchandiseController uploadMerch = new MerchandiseController(false);
+//         String[][] merchData = uploadMerch.getMerch();
+//         MerchandiseSale[] MerchSystemRecords = new MerchandiseSale[uploadMerch.getMerch().length];
+//         try
+//         {
+//             FileWriter Writer = new FileWriter(FilePathToRecords, true);
+//             for (int i = 0; i < merchData.length; i++) 
+//             {
+//                 float tempCost = (Float.parseFloat(merchData[i][3]) * Float.parseFloat(merchData[i][4])) - (Float.parseFloat(merchData[i][3]) * Float.parseFloat(merchData[i][5]));
+//                 MerchandiseSale merchSale = new MerchandiseSale(merchData[i][2].trim(),tempCost,YearMonth.parse(merchData[i][6].trim()));
+//                 MerchSystemRecords[i] = merchSale;
+//                 if(SendtoRecords)
+//                 {
+//                     Writer.write(MerchSystemRecords[i].name + ", " + MerchSystemRecords[i].cost + ", " + MerchSystemRecords[i].date + "\n");
                     
-                }
-                else 
-                {
-                    System.out.println(MerchSystemRecords[i].name + ", " + MerchSystemRecords[i].cost + ", " + MerchSystemRecords[i].date);
-                }
-            }
-            Writer.close();
-            System.out.println("Successfully retrieved all Merchandise Records");
-        }
-        catch (IOException ex) 
+//                 }
+//                 else 
+//                 {
+//                     System.out.println(MerchSystemRecords[i].name + ", " + MerchSystemRecords[i].cost + ", " + MerchSystemRecords[i].date);
+//                 }
+//             }
+//             Writer.close();
+//             System.out.println("Successfully retrieved all Merchandise Records");
+//         }
+//         catch (IOException ex) 
+//         {
+//             System.out.println("IOException is caught");
+//         }
+
+//         return MerchSystemRecords;
+
+//     }
+
+// }
+
+public ArrayList<MerchandiseSale> TurnAllMerchandiseToRecords(boolean sendToRecords)  //Makes object serializable in file
+{
+
+    MerchandiseController uploadMerch = new MerchandiseController(false);
+    String[][] merchData = uploadMerch.getMerch();
+
+    ArrayList<MerchandiseSale> merchList = new ArrayList<>();
+
+    for (int i = 0; i < merchData.length; i++) 
         {
-            System.out.println("IOException is caught");
+        float tempCost = (Float.parseFloat(merchData[i][3]) * Float.parseFloat(merchData[i][4])) - (Float.parseFloat(merchData[i][3]) * Float.parseFloat(merchData[i][5]));
+        MerchandiseSale merchSale = new MerchandiseSale(merchData[i][2].trim(), tempCost, YearMonth.parse(merchData[i][6].trim()));
+        merchList.add(merchSale);
+        if (!sendToRecords) 
+        {
+            System.out.println(merchSale.name + ", " + merchSale.cost + ", " + merchSale.date);
         }
+    }
+    if (sendToRecords) 
+        {
+            try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(FilePathToRecords))) {
+            out.writeObject(merchList);
 
-        return MerchSystemRecords;
+            System.out.println("Successfully saved all Merchandise Records");
 
+        } catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
     }
 
+    return merchList;
+}
+
+    public ArrayList<MerchandiseSale> readRecords() 
+    {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(FilePathToRecords))) 
+        {
+            ArrayList<MerchandiseSale> arr = (ArrayList<MerchandiseSale>) in.readObject();
+            for(MerchandiseSale m : arr)
+            {
+                System.out.println("Name: " + m.name + " Cost: " + m.cost + " Date: " + m.date);
+            }
+                
+            return arr;
+
+        } 
+        catch (Exception e) 
+        {
+        e.printStackTrace();
+        }
+
+        return new ArrayList<>();
+    }
 }
